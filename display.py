5e76e474ddb143b2361114578fc3402998f77245
vivek <85469668+nikamvijaya@users.noreply.github.com>
2024-02-26 22:55:46 +0530

    Update display.py

--- display.py
+++ display.py
@@ -29,21 +29,6 @@ def main():
         acceleration.main()
     elif selected_topic == "energy":
         energy.main()
-    import random
-
-print("Guess the Number!")
-number_to_guess = random.randint(1, 100)
-while True:
-    guess = int(input("Guess a number between 1 and 100: "))
-    if guess == number_to_guess:
-        print("Congratulations! You guessed the number!")
-        break
-    elif guess < number_to_guess:
-        print("Too low! Try again.")
-    else:
-        print("Too high! Try again.")
-
-
 main()
 if __name__ == "_main_":
     main()    
