3c8b8a0884c3d46a17405f891585c45b1d516425
vivek <85469668+nikamvijaya@users.noreply.github.com>
2024-02-26 22:52:51 +0530

    Update display.py

--- display.py
+++ display.py
@@ -12,16 +12,13 @@ def main():
     st.write( '''It doesn't matter how beautiful your theory is it doesn't matter how smart you are If it doesn't agree with experiment, it's wrong.''')
 
     st.header("HARMONIC OSCILLATOR")
-    col1, col2 , col3 = st.columns(3)
+    col1, col2  = st.columns(2)
     with col1:
         if st.button("spring oscilaton"):
             st.image('vivek.gif', caption='spring oscilaton')
     with col2:
         if st.button("coupled oscilation"):
             st.image('vivek2.gif', caption='coupled oscilaton')
-    with col3:
-        if st.button("vivek"):
-            st.image('IMG-20240222-WA0013.jpg', caption='vivu')
         
     selected_topic = st.selectbox("Choose a topic", ["displacement", "velocity", "acceleration", "energy"])
     if selected_topic == "displacement":
@@ -32,6 +29,22 @@ def main():
         acceleration.main()
     elif selected_topic == "energy":
         energy.main()
+    import random
+
+print("Guess the Number!")
+
+number_to_guess = random.randint(1, 100)
+
+while True:
+    guess = int(input("Guess a number between 1 and 100: "))
+    if guess == number_to_guess:
+        print("Congratulations! You guessed the number!")
+        break
+    elif guess < number_to_guess:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
+
 
 main()
 if __name__ == "_main_":
