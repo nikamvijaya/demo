49260a14c6a3496db78cbdc68daa316204c59f9c
vivek <85469668+nikamvijaya@users.noreply.github.com>
2024-02-26 22:54:23 +0530

    Update display.py

--- display.py
+++ display.py
@@ -32,9 +32,7 @@ def main():
     import random
 
 print("Guess the Number!")
-
 number_to_guess = random.randint(1, 100)
-
 while True:
     guess = int(input("Guess a number between 1 and 100: "))
     if guess == number_to_guess:
