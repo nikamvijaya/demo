862bc0499b188860cb085ff013f6214fd363f611
vivek <85469668+nikamvijaya@users.noreply.github.com>
2024-02-27 11:26:02 +0530

    Update display.py

--- display.py
+++ display.py
@@ -19,9 +19,6 @@ def main():
     with col2:
         if st.button("coupled oscilation"):
             st.image('vivek2.gif', caption='coupled oscilaton')
-    with col3:
-        if st.button("Type of oscilation"):
-            st.image('Screenshot_20240227_111306_Gallery', caption='Type of oscilaton')
     selected_topic = st.selectbox("Choose a topic", ["displacement", "velocity", "acceleration", "energy"])
     if selected_topic == "displacement":
         displacement.main()
